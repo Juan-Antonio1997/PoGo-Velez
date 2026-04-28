35c1bab404e9921df1db738eb05c598fbdb01c89
jgommar464 <jgommar464@g.educaand.es>
2026-04-28 13:58:47 +0200

    WIP: List template in the home page and home page design

--- index.php
+++ index.php
@@ -3,19 +3,58 @@ session_start();
 ?>
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PoGO Vélez-Málaga</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
+
 <body>
-    <?php if (isset($_SESSION['usuario'])): ?>
-        <p>¡Hola <?=$_SESSION['usuario']?>!</p>
-        <p><a href="./logout">Cerrar sesión</a></p>
-    <?php else: ?>
-        <p>¡Hola invitad@!</p>
-        <p><a href="./login">Iniciar sesión</a></p>
-        <p><a href="./registro">Regístrate</a></p>
-    <?php endif; ?>
+    <header>
+        <h1 class="pageTitle">PoGo Vélez-Málaga</h1>
+    </header>
+    <nav>
+        <?php if (isset($_SESSION['usuario'])): ?>
+            <ul class="navList">
+                <li class="navElement"><a href="./logout">Cerrar sesión</a></li>
+                <li class="navElement"><span>¡Hola <?= $_SESSION['usuario'] ?>!</span></li>
+            </ul>
+        <?php else: ?>
+            <ul class="navList">
+                <li class="navElement"><a href="./registro">Regístrate</a></li>
+                <li class="navElement"><a href="./login">Iniciar sesión</a></li>
+            </ul>
+        <?php endif; ?>
+    </nav>
+    <section>
+        <button class="createList">Crear lista</button>
+        <article>
+            <a href="#" class="listLink">
+                <div class="pokeList">
+                    <div class="pokemonSprite">
+                        <img src="media/pokemon/0381-0.png" width="150">
+                    </div>
+                    <div class="pokemonName">
+                        <span>Latios</span>
+                    </div>
+                    <div class="placeName">
+                        <span>Caballo Pegaso</span>
+                    </div>
+                    <div class="meetTime">
+                        <span>12:15</span>
+                    </div>
+                    <div class="lobbyStatus">
+                        <span>0/20</span>
+                    </div>
+                </div>
+            </a>
+        </article>
+    </section>
+    <footer>
+
+    </footer>
 </body>
+
 </html>
