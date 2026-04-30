e9de41ec1a0c310b24043e0732090721584596a7
jgommar464 <jgommar464@g.educaand.es>
2026-04-30 12:56:21 +0200

    feat: Listas en home + test playground

--- crearlista/index.php
+++ crearlista/index.php
@@ -22,7 +22,7 @@ if ($db) {
 
 <body>
     <header>
-        <h1 class="pageTitle">PoGo Vélez-Málaga</h1>
+        <a href="./" class="titleLink"><h1 class="pageTitle">PoGo Vélez-Málaga</h1></a>
     </header>
     <nav>
         <?php if (isset($_SESSION['usuario'])): ?>
--- index.php
+++ index.php
@@ -1,5 +1,28 @@
 <?php
+require_once('./config.php');
+require_once('./db_pdo.php');
+$db = db_open();
 session_start();
+if ($db) {
+    $listas = db_query($db, "SELECT l.ID_Lista, l.Ubicacion, l.Hora_quedada, i.ID_Pokemon, i.Tipo_Raid, i.Maximo_participantes, i.Maximo_remotos, p.Nombre 
+    FROM listas AS l
+    INNER JOIN incursiones AS i ON l.ID_Raid = i.ID_Raid
+    INNER JOIN pokemon AS p ON i.ID_Pokemon = p.ID_Pokemon
+    WHERE DATE_ADD(l.Hora_quedada, INTERVAL 1 MINUTE) > NOW()
+    ORDER BY l.Hora_quedada;");
+    $hoy = date("d/m/Y");
+    for ($i = 0; $i < count($listas); $i++) {
+        $participantes = db_query($db, "SELECT COUNT(Username) AS Num_Apuntados
+        FROM apuntados_lista WHERE ID_Lista = (?)", [$listas[$i]['ID_Lista']]);
+        $listas[$i]['Participantes'] = $participantes[0]['Num_Apuntados'];
+        $remotos = db_query($db, "SELECT COUNT(Username) AS Num_Remotos
+        FROM apuntados_lista WHERE ID_Lista = (?) AND Pase = (?)", [$listas[$i]['ID_Lista'], 'Remoto']);
+        $listas[$i]['Remotos'] = $remotos[0]['Num_Remotos'];
+        $fecha = strtotime($listas[$i]['Hora_quedada']);
+        $listas[$i]['fecha'] = date("d/m/Y", $fecha);
+        $listas[$i]['hora'] = date("H:i", $fecha);
+    }
+}
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -13,7 +36,9 @@ session_start();
 
 <body>
     <header>
-        <h1 class="pageTitle">PoGo Vélez-Málaga</h1>
+        <a href="./" class="titleLink">
+            <h1 class="pageTitle">PoGo Vélez-Málaga</h1>
+        </a>
     </header>
     <nav>
         <?php if (isset($_SESSION['usuario'])): ?>
@@ -31,25 +56,35 @@ session_start();
     <section>
         <button class="createList">Crear lista</button>
         <article>
-            <a href="#" class="listLink">
-                <div class="pokeList">
-                    <div class="pokemonSprite">
-                        <img src="media/pokemon/0381-0.png" width="150">
-                    </div>
-                    <div class="pokemonName">
-                        <span>Latios</span>
-                    </div>
-                    <div class="placeName">
-                        <span>Caballo Pegaso</span>
-                    </div>
-                    <div class="meetTime">
-                        <span>12:15</span>
-                    </div>
-                    <div class="lobbyStatus">
-                        <span>0/20</span>
-                    </div>
-                </div>
-            </a>
+            <?php if (!empty($listas)): ?>
+                <?php foreach ($listas as $lista): ?>
+                    <a href="lista?id=<?= $lista['ID_Lista'] ?>" class="listLink">
+                        <div class="pokeList">
+                            <div class="pokemonSprite">
+                                <img src="media/pokemon/<?= $lista['ID_Pokemon'] ?>.png" width="150">
+                            </div>
+                            <div class="pokemonName">
+                                <span><?= $lista['Nombre'] ?></span>
+                            </div>
+                            <div class="placeName">
+                                <span><?= $lista['Ubicacion'] ?></span>
+                            </div>
+                            <?php if ($lista['fecha'] == $hoy): ?>
+                                <div class="meetTime">
+                                    <span><?= $lista['hora'] ?></span>
+                                </div>
+                            <?php else: ?>
+                                <div class="meetTime">
+                                    <span><?= $lista['fecha'] ?> - <?= $lista['hora'] ?></span>
+                                </div>
+                            <?php endif; ?>
+                            <div class="lobbyStatus">
+                                <span><?= $lista['Participantes'] ?>/<?= $lista['Maximo_participantes'] ?> (<?= $lista['Remotos'] ?>/<?= $lista['Maximo_remotos'] ?> remotos)</span>
+                            </div>
+                        </div>
+                    </a>
+                <?php endforeach; ?>
+            <?php endif; ?>
         </article>
     </section>
     <footer>
